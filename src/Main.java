0d428394c975784ed2096b518b7f508758a8108f
noamsavranski <noamsavran1@gmail.com>
2025-03-03 20:25:01 +0200

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,48 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        task t1 = new task("brush my hair");
-        System.out.println(t1.toString());
-        t1.markAsComplete();
-        System.out.println(t1.toString());
+        Scanner scanner = new Scanner(System.in);
+        toDoList list = new toDoList();
+        int action = 0;
+        while (action != 5) {
+            System.out.println("what do you want to do?");
+            System.out.println("add a task -press 1");
+            System.out.println("complete a task - press 2");
+            System.out.println("remove a task - press 3");
+            System.out.println("show all tasks - press 4");
+            System.out.println("exit - press 5");
+            action = scanner.nextInt();
+            scanner.nextLine();
+
+            if (action < 1 || action > 5){
+                System.out.println("number is invalid, try again");
+            }
+
+            else if (action == 1) {
+                System.out.print("Enter task description: ");
+                String description = scanner.nextLine();
+                list.addTask(description);
+            }
+            else if (action == 2) {
+                System.out.println("Enter the task number: ");
+                int toComplete = scanner.nextInt();
+                list.competeTask(toComplete - 1);
+            }
+
+            else if (action == 3) {
+                System.out.println("Enter the task number: ");
+                int toRemove = scanner.nextInt();
+                list.removeTask(toRemove - 1);
+            }
+
+            else if (action == 4){
+                list.showTasks();
+            }
+
+        }
 
     }
 }
--- src/toDoList.java
+++ src/toDoList.java
@@ -1,5 +1,47 @@
 import java.util.LinkedList;
 
 public class toDoList {
-    private LinkedList
+    private LinkedList<task> toDo;
+
+    public toDoList() {
+        this.toDo = new LinkedList<task>();
+    }
+    public void addTask(String description) {
+        task t = new task(description);
+        this.toDo.add(t);
+        System.out.println("Task added!");
+    }
+
+    public void competeTask(int index){
+        if (toDo.size() <= index || index < 0){
+            System.out.println("index cant be found in the list" );
+        }
+        else {
+            this.toDo.get(index).markAsComplete();
+            System.out.println("task completed!");
+        }
+    }
+
+    public void removeTask(int index) {
+        if (toDo.size() <= index || index < 0) {
+            System.out.println("index cant be found in the list");
+        }
+        else {
+            this.toDo.remove(index);
+            System.out.println("TASK DELETED!");
+        }
+    }
+    public void showTasks(){
+        if (toDo.isEmpty()) {
+            System.out.println("list is empty:)");
+        }
+        else {
+            System.out.println("TASKS:");
+            int index = 1;
+            for (task t : this.toDo) {
+                System.out.println(index + "." + t);
+                index = index + 1;
+            }
+        }
+    }
 }
